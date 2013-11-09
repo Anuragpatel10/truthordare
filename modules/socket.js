5baa0a4ed95eec6efa86776cdb9fc5d4dc80b12e
Suroor Wijdan <suroor@intelligrape.com>
2013-11-09 18:07:54 +0530

    Added socket code for interaction with server

--- modules/socket.js
+++ modules/socket.js
@@ -1,18 +1,21 @@
-var uid = require('uuid');
+var uid = require('uid');
 exports = module.exports = function () {
 
     io.sockets.on('connection', function (socket) {
-        socket.on("id", function () {
-            var id=uid.v4();
-            console.log("------------Socket Room " + id + "Created for User -------------");
+        socket.on("id", function (name) {
+            var id = uid(7);
+            console.log("Socket Room Created ---", id);
+            console.log("Users in Room ---", io.sockets.clients(id));
             socket.join(id);
-            socket.emit("gameInitiated", id);
+            socket.emit("gameInitiated", {name: name, id: id});
         });
 
 
-        socket.on("joinGame", function(data){
-          socket.join(data);
-          socket.emit("joinedGame", data.name);
+        socket.on("joinGame", function (data) {
+            socket.join(data.token);
+            io.sockets.in(data.token).emit("newPlayerJoined", data);
+            console.log("Users in Room ---", io.sockets.clients(data.token));
+            socket.emit("joinedGame", data);
         });
     });
 
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,20 +2,24 @@ var script  = (function(){
     var script = {};
 
     script.domElementsEnum = {
-        "GAME_ID" : "#gameRoomId"
+        "GAME_ID" : "#gameRoomId",
+        "USER_NAME" : "#gameInitNameField"
     };
 
     script.actions = {
         joinARoom: function(){
             var roomId = $(script.domElementsEnum.GAME_ID).val();
-            console.log("Joining a Room...");
             if(roomId){
+                console.log("Joining a Room...");
                 script.socket.emit("joinGame", roomId);
             }
         },
         startARoom: function(){
-            console.log("Starting Game...");
-            script.socket.emit("id");
+            var username = $(script.domElementsEnum.USER_NAME).val();
+            if(username){
+                console.log("Starting Game...");
+                script.socket.emit("newGameRequest", username);
+            }
         },
         openGamePopup: function(){
             $("#gameInitPopup").show();
@@ -27,6 +31,14 @@ var script  = (function(){
 
         script.socket.on("gameInitiated", function(roomId){
             console.log(roomId);
+        });
+
+        script.socket.on("newPlayerJoined", function(data){
+            console.log(data);
+        });
+
+        script.socket.on("joinedGame", function(data){
+            console.log(data);
         });
     };
 
