e38bcc2268e6e60af104ee084dc6d8a29e62b2d1
Suroor Wijdan <suroor@intelligrape.com>
2013-11-09 18:48:36 +0530

    Implemented the socket room creation code

--- modules/socket.js
+++ modules/socket.js
@@ -1,9 +1,11 @@
 var uid = require('uid');
 exports = module.exports = function () {
 
+    var usersInRoom = {};
     io.sockets.on('connection', function (socket) {
-        socket.on("id", function (name) {
+        socket.on("newGameRequest", function (name) {
             var id = uid(7);
+            usersInRoom[name] = id;
             console.log("Socket Room Created ---", id);
             console.log("Users in Room ---", io.sockets.clients(id));
             socket.join(id);
@@ -13,8 +15,8 @@ exports = module.exports = function () {
 
         socket.on("joinGame", function (data) {
             socket.join(data.token);
-            io.sockets.in(data.token).emit("newPlayerJoined", data);
-            console.log("Users in Room ---", io.sockets.clients(data.token));
+            usersInRoom[data.name] = data.token;
+            io.sockets.in(data.token).emit("newPlayerJoined", usersInRoom);
             socket.emit("joinedGame", data);
         });
     });
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,83 +1,79 @@
-var script  = (function(){
+var script = (function () {
     var script = {};
 
     script.domElementsEnum = {
-        "GAME_ID" : "#gameRoomId",
-        "USER_NAME" : "#gameInitNameField"
+        "GAME_ID": "#gameRoomId",
+        "USER_NAME": "#gameInitNameField",
+        "JOIN_USERNAME": "#gameJoinNameField",
+        "JOIN_TOKEN": "#gameJoinTokenField"
     };
 
     script.actions = {
-        joinARoom: function(){
-            var roomId = $(script.domElementsEnum.GAME_ID).val();
-            if(roomId){
-                console.log("Joining a Room...");
-                script.socket.emit("joinGame", roomId);
-            }
-            $("#gameJoinPopupContainer").show();
-            $("#gameJoinNameField").focus();
+        openGamePopup: function () {
+            $("#gameInitPopupContainer").show();
+            $("#gameInitNameField").focus();
         },
-        startARoom: function(){
-            var username = $(script.domElementsEnum.USER_NAME).val();
-            if(username){
+        closeGamePopup: function () {
+            $("#gameInitPopupContainer").hide();
+        },
+        closeJoinGamePopup: function () {
+            $("#gameJoinPopupContainer").hide();
+        },
+        processInitGame: function () {
+            var username = $("#gameInitNameField").val();
+            if (username) {
                 console.log("Starting Game...");
                 script.socket.emit("newGameRequest", username);
             }
+            //script.actions.closeGamePopup();
         },
-        openGamePopup: function(){
-            $("#gameInitPopupContainer").show();
-            $("#gameInitNameField").focus();
+        processJoinGame: function () {
+            var roomId = $("#gameJoinTokenField").val();
+            var username = $("#gameJoinNameField").val();
+            if (roomId) {
+                console.log("Joining a Room...");
+                script.socket.emit("joinGame", {token: roomId, name:username});
+            }
+            //script.actions.closeJoinGamePopup();
         },
-        closeGamePopup: function(){
-            $("#gameInitPopupContainer").hide();
-        },
-        closeJoinGamePopup: function(){
-            $("#gameJoinPopupContainer").hide();
-        },
-        processInitGame: function(){
-            script.actions.closeGamePopup();
-        },
-        processJoinGame: function(){
-            script.actions.closeJoinGamePopup();
-        },
-        openJoinGamePopup: function(){
-            $("#gameInitPopupContainer").show();
-            $("#gameInitNameField").focus();
+        openJoinGamePopup: function () {
+            $("#gameJoinPopupContainer").show();
+            $("#gameJoinNameField").focus();
         }
     };
 
-    script.socketInitialize = function(){
-        script.socket = io.connect('http://localhost:8000');
+    script.socketInitialize = function () {
+        script.socket = io.connect('http://10.1.1.69:8000');
 
-        script.socket.on("gameInitiated", function(roomId){
+        script.socket.on("gameInitiated", function (roomId) {
             console.log(roomId);
         });
 
-        script.socket.on("newPlayerJoined", function(data){
+        script.socket.on("newPlayerJoined", function (data) {
             console.log(data);
         });
 
-        script.socket.on("joinedGame", function(data){
+        script.socket.on("joinedGame", function (data) {
             console.log(data);
         });
     };
 
-    script.bindEventHandlers = function(){
-        $("#startAGame").on("click", script.actions.startARoom);
-        $("#joinAGame").on("click", script.actions.joinARoom);
+    script.bindEventHandlers = function () {
         $("#initGame").on("click", script.actions.openGamePopup);
+        $("#joinAGame").on("click", script.actions.openJoinGamePopup);
         $("#gameSubmit").on("click", script.actions.processInitGame);
-        $("#cancelGameSubmit").on("click", script.actions.closeGamePopup);
         $("#gameJoinSubmit").on("click", script.actions.processJoinGame);
         $("#cancelJoinGameSubmit").on("click", script.actions.closeJoinGamePopup);
+        $("#cancelGameSubmit").on("click", script.actions.closeGamePopup);
     };
 
-    script.constructor = function(){
+    script.constructor = function () {
         script.bindEventHandlers();
         script.socketInitialize();
     };
 
-    script.init = function(){
-        $(document).on("ready", function(){
+    script.init = function () {
+        $(document).on("ready", function () {
             script.constructor();
         })
     };
